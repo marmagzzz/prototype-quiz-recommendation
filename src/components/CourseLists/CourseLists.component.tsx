a252fd30d361965e8282c231e5dd96f32dbd5e2f
Marvin Magnaye <marvinmagnaye14@gmail.com>
2023-09-22 02:26:59 +0800

    Enh: Added course lists component

---
+++ src/components/CourseLists/CourseLists.component.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import styles from './CourseLists.module.scss';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import { TCourse } from '@/types';
+
+type TCourseListsProp = {
+    courseLists: TCourse[];
+};
+
+export default function CourseLists({ courseLists }: TCourseListsProp) {
+    return (
+        <section className={`${styles.sectionContainer}`}>
+            <Container>
+                <h1>Available Courses</h1>
+                <Row xs={1} lg={5} className='g-4'>
+                    {courseLists.map((courseObj, index) => (
+                        <Col key={index}>
+                            <Card>
+                                <Card.Img
+                                    variant='top'
+                                    src='https://placeholder.co/250x250/png'
+                                />
+                                <Card.Body>
+                                    <Card.Title>{courseObj.code}</Card.Title>
+                                    <Card.Text>{courseObj.subText}</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+        </section>
+    );
+}
