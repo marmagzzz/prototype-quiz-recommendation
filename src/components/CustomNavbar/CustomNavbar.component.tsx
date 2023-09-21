df2cc25f2556793d927f40df15ab4be74a990ec2
Marvin Magnaye <marvinmagnaye14@gmail.com>
2023-09-22 02:27:18 +0800

    Enh: Custom nav bar

---
+++ src/components/CustomNavbar/CustomNavbar.component.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+export default function CustomNavbar() {
+    return (
+        <Navbar expand='lg' className='bg-body-tertiary' fixed='top'>
+            <Container>
+                <Navbar.Brand href='/'>Quiz</Navbar.Brand>
+
+                <Navbar.Toggle aria-controls='basic-navbar-nav' />
+
+                <Navbar.Collapse id='basic-navbar-nav'>
+                    <Nav className='me-auto'>
+                        <Nav.Link href='/'>Home</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
