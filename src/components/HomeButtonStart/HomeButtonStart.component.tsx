df7f2c386aa21f240c8a726856d15aa493814cfb
Marvin Magnaye <marvinmagnaye14@gmail.com>
2023-09-29 15:07:14 +0800

    Fix: Removed unused btn component.

--- src/components/HomeButtonStart/HomeButtonStart.component.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { Button } from 'react-bootstrap';
-
-import styles from './HomeButtonStart.module.scss';
-
-export default function HomeButtonStart() {
-    return <Button />;
-}
